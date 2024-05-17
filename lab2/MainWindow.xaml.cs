dc3253f29412a6ca0647835250329d8461f73ff1
Denys <feed.exfoss@gmail.com>
2024-05-17 22:04:37 +0300

    zavd 4 cs

--- lab2/MainWindow.xaml.cs
+++ lab2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,61 @@ using System.Windows.Shapes;
 
 namespace lab2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var saveCommand = new CommandBinding(ApplicationCommands.Save, Executed_Save, CanExecute_Save);
+            var openCommand = new CommandBinding(ApplicationCommands.Open, Executed_Open, CanExecute_Open);
+            var eraseCommand = new CommandBinding(ApplicationCommands.New, Executed_Erase, CanExecute_Erase);
+            CommandBindings.Add(saveCommand);
+            CommandBindings.Add(openCommand);
+            CommandBindings.Add(eraseCommand);
+        }
+
+        private void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = InputTextBox.Text.Trim().Length > 0;
+        }
+
+        private void Executed_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt"
+            };
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                System.IO.File.WriteAllText(saveFileDialog.FileName, InputTextBox.Text);
+            }
+        }
+
+        private void CanExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Executed_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                InputTextBox.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+        private void CanExecute_Erase(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = InputTextBox.Text.Trim().Length > 0;
+        }
+
+        private void Executed_Erase(object sender, ExecutedRoutedEventArgs e)
+        {
+            InputTextBox.Text = "";
         }
     }
 }
+
