734eb38e255d71423303f8627ecbc09fb1e65769
Denys <feed.exfoss@gmail.com>
2024-05-19 20:56:18 +0300

    CRUD operations implemented

--- lab4/DatabaseAccess.cs
+++ lab4/DatabaseAccess.cs
@@ -34,5 +34,61 @@ namespace lab4
 
             return booksTable;
         }
+
+        public void AddBook(string isbn, string name, string author, string publisher, int year)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Books (ISBN, Name, Author, Publisher, Year) VALUES (@ISBN, @Name, @Author, @Publisher, @Year)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ISBN", isbn);
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Author", author);
+                    command.Parameters.AddWithValue("@Publisher", publisher);
+                    command.Parameters.AddWithValue("@Year", year);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateBook(string isbn, string name, string author, string publisher, int year)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Books SET Name = @Name, Author = @Author, Publisher = @Publisher, Year = @Year WHERE ISBN = @ISBN";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ISBN", isbn);
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Author", author);
+                    command.Parameters.AddWithValue("@Publisher", publisher);
+                    command.Parameters.AddWithValue("@Year", year);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteBook(string isbn)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Books WHERE ISBN = @ISBN";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ISBN", isbn);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
--- lab4/MainWindow.xaml.cs
+++ lab4/MainWindow.xaml.cs
@@ -13,25 +13,96 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace lab4
 {
     public partial class MainWindow : Window
     {
         private DatabaseAccess databaseAccess;
+        public DataView BooksTable { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
             databaseAccess = new DatabaseAccess();
-            LoadData();
         }
 
-        private void LoadData()
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             DataTable booksTable = databaseAccess.GetBooks();
-            // Assuming you have a DataGrid named BooksDataGrid in your XAML
-            BooksDataGrid.ItemsSource = booksTable.DefaultView;
+            if (booksTable != null && booksTable.Rows.Count > 0)
+            {
+                BooksTable = booksTable.DefaultView;
+                listBooks.ItemsSource = BooksTable;
+                listBooks.SelectedIndex = 0;
+                listBooks.Focus();
+            }
+            else
+            {
+                MessageBox.Show("No data found in the Books table.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string isbn = txtISBN.Text;
+                string name = txtName.Text;
+                string author = txtAuthor.Text;
+                string publisher = txtPublisher.Text;
+                int year = int.Parse(txtYear.Text);
+
+                databaseAccess.AddBook(isbn, name, author, publisher, year);
+                RefreshData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error creating book: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string isbn = txtISBN.Text;
+                string name = txtName.Text;
+                string author = txtAuthor.Text;
+                string publisher = txtPublisher.Text;
+                int year = int.Parse(txtYear.Text);
+
+                databaseAccess.UpdateBook(isbn, name, author, publisher, year);
+                RefreshData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating book: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string isbn = txtISBN.Text;
+                databaseAccess.DeleteBook(isbn);
+                RefreshData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error deleting book: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void RefreshData()
+        {
+            DataTable booksTable = databaseAccess.GetBooks();
+            BooksTable = booksTable.DefaultView;
+            listBooks.ItemsSource = BooksTable;
+            listBooks.SelectedIndex = 0;
+            listBooks.Focus();
         }
     }
 }
